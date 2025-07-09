5a8e41d696cefd520a78e07808d65478c1dbb56d
SyedSammad-123 <syedsammadjamshad@gmail.com>
2025-07-09 21:36:29 +0500

    commit

---
+++ app.js
@@ -0,0 +1,65 @@
+console.log("Syed samad")
+alert("Welcome JavaScript");
+
+
+document.write("<h2>JavaScript is a high-level, often just-in-time compiled, language that is primarily used to create interactive and dynamic content on websites</h2>")
+
+
+var userName = "Syed Samad"
+console.log(userName);
+
+a = 10
+console.log(a);
+
+
+a = 20
+console.log(a);
+
+
+var camelCase="it Is A Camel Case Example"
+console.log(camelCase);
+
+var snake_case="it is a snake_case Example"
+console.log(snake_case);
+
+var PascalCase="It Is A Pascal Case Example"
+console.log(PascalCase);
+
+//All Data Types
+
+var name = "Syed samad"
+console.log(typeof name, name);
+
+var Age = 16
+console.log(typeof Age, Age);
+
+
+var Male=true
+var Female=false
+
+console.log(typeof Male, Male)
+console.log(typeof Female, Female)
+
+var uncheck = undefined
+console.log(typeof uncheck, uncheck)
+
+
+//Math Expression
+
+//Add +
+
+var a=32;
+var b=22;
+
+var Add=a+b;
+
+console.log(Add);
+
+//Sub -
+
+var x=62;
+var y=88;
+
+var Sub=x-y;
+
+console.log(Sub);
